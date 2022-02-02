59d6a3d53852c05ec0d9a2bd734ae14c011857c2
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-02 19:00:03 +0100

    Add word decoder

--- main.rb
+++ main.rb
@@ -1,11 +1,27 @@
 @to_english = {
   '.-' => 'A',
   '-...' => 'B',
-  '-.-.' => 'C'
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K'
 }
 
 def char_decode(str)
-  puts @to_english[str]
+  @to_english[str]
 end
 
 char_decode('-.-.')
+
+# Decode words
+
+def word_decode(str)
+  str.split.map { |i| char_decode(i) }.join
+end
+
+puts word_decode('.- -... -.-.')
